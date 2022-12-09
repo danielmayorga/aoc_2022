eed39ffaf1784c003835d302cc4ce5847ab2413c
Daniel Mayorga <danielmayorga@daniels-air.lan>
2022-12-09 08:09:05 -0800

    refactoring day 9 to be less repetative.

--- src/day09.ts
+++ src/day09.ts
@@ -16,41 +16,57 @@ const commands: Command[] = fileRaw.split(/\r?\n/).map(line => {
     return {direction, position: Number(position)} as Command;
 });
 
-function part1(command: Command[]){
+//how the tail(or next knot in part 2) responds to the previous knot moving
+function move(head: Position, tail: Position){
+    let diffX = head.x - tail.x;
+    let diffY = head.y - tail.y;
+
+    if (Math.abs(diffX) <2 && Math.abs(diffY) < 2){
+        return ;
+    }
+
+    if (diffX === 0){
+        tail.y+= diffY > 0 ? 1 : -1;
+    } else if (diffY === 0){
+        tail.x+= diffX > 0 ? 1 : -1;
+    } else if (Math.abs(diffX) === 1){//diagonal 
+        tail.x = head.x;
+        tail.y+= diffY > 0 ? 1 : -1;
+    }else if (Math.abs(diffY) === 1){
+        tail.x+= diffX > 0 ? 1 : -1;
+        tail.y = head.y;
+    }else if (Math.abs(diffX) === 2 && Math.abs(diffY) === 2){
+        //new condition, you can move diagonally now
+        tail.x+= diffX > 0 ? 1 : -1;
+        tail.y+= diffY > 0 ? 1 : -1;
+    }else{
+        throw new Error("Shouldn't get here")
+    }
+}
+
+//solution to part 1 and 2 just update parameter
+function solution(commands: Command[], knotCount: number){
     let visited = new Set<string>();
     function setVisited(tail: Position){
         visited.add(String(tail.x)+","+String(tail.y));
     }
     
-    const head: Position = { x: 0, y: 0};
-    const tail: Position = { x: 0, y: 0};
+    const knots: Position[] = new Array(knotCount);
+    for (let i=0; i<knotCount; i++){
+        knots[i] = { x: 0, y: 0};
+    }
+    const head = knots[0];
+    const tail = knots[knotCount-1];
     setVisited(tail);//starting position is visited
-
     function update(headDx: number, headDy: number, times: number){
         for(let time=0; time < times; time++){
             //update head
             head.x+=headDx;
             head.y+=headDy;
-            //update tail
-            let diffX = head.x - tail.x;
-            let diffY = head.y - tail.y;
-
-            if (Math.abs(diffX) <2 && Math.abs(diffY) < 2){
-                continue; //nothing to see here we wont change tail
+            //update knots
+            for(let i=1; i<knotCount; i++){
+                move(knots[i-1], knots[i]);
             }
-
-            if (diffX === 0){
-                tail.y+= diffY > 0 ? 1 : -1;
-            } else if (diffY === 0){
-                tail.x+= diffX > 0 ? 1 : -1;
-            } else if (Math.abs(diffX) === 1){//diagonal 
-                tail.x = head.x;
-                tail.y+= diffY > 0 ? 1 : -1;
-            }else {
-                tail.x+= diffX > 0 ? 1 : -1;
-                tail.y = head.y;
-            }
-
             //set visited
             setVisited(tail);
         }
@@ -78,84 +94,7 @@ function part1(command: Command[]){
     return visited.size;
 }
 
-function part2(command: Command[]){
-    let visited = new Set<string>();
-    function setVisited(tail: Position){
-        visited.add(String(tail.x)+","+String(tail.y));
-    }
-    
-    const knots: Position[] = new Array(10);
-    for (let i=0; i<10; i++){
-        knots[i] = { x: 0, y: 0};
-    }
-    const head = knots[0];
-    const tail = knots[9];
-    setVisited(tail);//starting position is visited
-
-    function updateHelper(head: Position, tail: Position){
-        let diffX = head.x - tail.x;
-        let diffY = head.y - tail.y;
-
-        if (Math.abs(diffX) <2 && Math.abs(diffY) < 2){
-            return ;
-        }
-
-        if (diffX === 0){
-            tail.y+= diffY > 0 ? 1 : -1;
-        } else if (diffY === 0){
-            tail.x+= diffX > 0 ? 1 : -1;
-        } else if (Math.abs(diffX) === 1){//diagonal 
-            tail.x = head.x;
-            tail.y+= diffY > 0 ? 1 : -1;
-        }else if (Math.abs(diffY) === 1){
-            tail.x+= diffX > 0 ? 1 : -1;
-            tail.y = head.y;
-        }else if (Math.abs(diffX) === 2 && Math.abs(diffY) === 2){
-            //new condition, you can move diagonally now
-            tail.x+= diffX > 0 ? 1 : -1;
-            tail.y+= diffY > 0 ? 1 : -1;
-        }else{
-            throw new Error("Shouldn't get here")
-        }
-    }
-
-    function update(headDx: number, headDy: number, times: number){
-        for(let time=0; time < times; time++){
-            //update head
-            head.x+=headDx;
-            head.y+=headDy;
-            //update tail
-            for(let i=1; i<10; i++){
-                updateHelper(knots[i-1], knots[i]);
-            }
-            //set visited
-            setVisited(tail);//2573
-        }
-    }
-
-    for (let command of commands){
-        switch(command.direction){
-            case 'U':
-                update(0,1, command.position);
-            break;
-            case 'D':
-                update(0,-1, command.position);
-            break;
-            case 'L':
-                update(-1,0, command.position);
-            break;
-            case 'R':
-                update(1, 0, command.position);
-            break;
-            default:
-                throw new Error("Invalid Direction : "+ command.direction);
-        }
-    }
-
-    return visited.size;
-}
-
-const answer1 = part1(commands);
-const answer2 = part2(commands);
+const answer1 = solution(commands,2);
+const answer2 = solution(commands,10);
 
 console.log(`part 1: ${answer1}\npart 2: ${answer2}`)
